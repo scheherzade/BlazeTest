59e6a50e31388b5b25b13c06f419df04321f8eee
Shahrzad Shirzad <shahrzadshirzad@gmail.com>
2019-11-05 17:08:58 -0600

    Adding BBLAS studd

--- main.cpp
+++ main.cpp
@@ -3,11 +3,16 @@
 #include <blaze/Math.h>
 #include <cstdlib>
 #include <iostream>
-
-using namespace blaze;
+#include <boost/graph/graphviz.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/graph/adjacency_list.hpp>
 #include <variant>
 #include <string>
-#include <hpx/hpx_main.hpp>
+//#include <hpx/hpx_main.hpp>
+#include <boost/graph/depth_first_search.hpp>
+//#include <blaze/math/typetraits/getMflop.h>
+using namespace blaze;
+
 
 int main() {
 
@@ -25,20 +30,38 @@ int main() {
     std::cout << "None" << std::endl;
 #endif
 
+//    std::cout << "Using Boost "
+//              << BOOST_VERSION / 100000     << "."  // major version
+//              << BOOST_VERSION / 100 % 1000 << "."  // minor version
+//              << BOOST_VERSION % 100                // patch level
+//              << std::endl;
+//
 
-    std::size_t n = 1000;
-    blaze::DynamicMatrix<double> C(n, n, 2.0);
+    std::size_t n = 913;
+//    blaze::DynamicMatrix<double, true> C(n, n, 2.0);
     blaze::DynamicMatrix<double> B(n, n, 3.0);
-    blaze::DynamicMatrix<double> D(n, n, 0.0);
-    blaze::DynamicMatrix<double> E(n, n, 0.0);
+//    blaze::DynamicMatrix<double> D(n, n, 0.0);
+//    blaze::DynamicMatrix<double, true> E(n, n, 4.0);
+//    blaze::DynamicVector<double> VB(1000000, 3.0);
 
-    auto k = B * C;
-    D = k;
+    blaze::DynamicVector<double> V1(24, 2.0);
+    std::iota(V1.begin(),V1.end(),0.);
+    std::cout<<V1<<std::endl;
+    blaze::CustomMatrix<double,blaze::unaligned,blaze::unpadded> tmp(&V1[0],4,6);
+    std::cout<<tmp<<std::endl;
+    auto k = B * B ;
+//    std::cout<< typeid(k).name()<<std::endl;
+    B = k;
+//    blaze::DynamicMatrix<int, blaze::rowMajor> M{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//    blaze::DynamicMatrix<int, blaze::columnMajor> Mt(M);
 
-    auto m = B + C;
-    E = m;	
-
+//    std::cout << blaze::getTotalMflop(M * M * M) << std::endl;         //  90 ok
+//    std::cout << blaze::getTotalMflop(Mt * Mt * Mt) << std::endl;      //  90 ok
+//    std::cout << blaze::getTotalMflop(M * (M + M)) << std::endl;       //  54 ok
+//    std::cout << blaze::getTotalMflop((M + M) * M) << std::endl;       //  54 ok
+//    std::cout << blaze::getTotalMflop((M + M) * (M + M)) << std::endl; //  63
+//    std::cout << blaze::getTotalMflop(M * M + M * M) << std::endl;     //  9 not ok should be 99 !!!
+//    std::cout << blaze::getTotalMflop(5 * M + M * M) << std::endl;     //  63
     return 0;
 }
-
 
